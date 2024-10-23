4a18d19b251e6ca75fe5d0ebaa4dc2204537d5ba
Андрей Боев <a.boev.it@gmail.com>
2024-10-23 17:19:22 +0300

    FIX: Задание 15

--- Тема 15/Задание 15(II).py
+++ Тема 15/Задание 15(II).py
@@ -6,11 +6,11 @@
 тождественно истинно, то есть принимает значение 1 при любом значении переменной х.
 """
 
-p = range(12, 63)
+p = range(12, 62)
 q = range(52, 92)
 min_len = 100
 for begin in range(100):
-    for end in range(100):
+    for end in range(begin, 100):
         a = range(begin, end)
         if all(not (not(x in a) and (x in p)) or (x in q) for x in range(100)):
             min_len = min(min_len, end - begin)
---
+++ Тема 16/Задание 16 (hard).py
@@ -0,0 +1,43 @@
+"""
+Алгоритм вычисления значения функции F(n), где n — целое число, задан следующими соотношениями:
+
+F(n) = n, если n < 10
+F(n) = F(n mod 10) + F(n div 10), если n >= 10.
+
+Определите количество значений n, меньших 2^63, для которых F(n) = 159.
+
+Ответ: 
+"""
+from functools import cache
+
+"""
+from functools import *
+
+# граничное значение
+a = list(map(int, str(2 ** 63 - 1)))
+
+
+@cache
+def f(s, l, fl):
+    if l == 0:
+        return s == 159
+    # проверяем ограниченные подпоследовательности большей длины
+    return sum(f(s + x, l - 1, fl and (x == a[-l])) for x in range([10, a[-l] + 1][fl]))
+
+print(f(0, len(a), 1))
+"""
+
+"""
+def f(n):
+    if n < 10:
+        return n
+    else:
+        return f(n % 10) + f(n // 10)
+
+
+count = 0
+for n in range(2 ** 63 - 1):
+    if f(n) == 159:
+        count += 1
+print(count)
+"""
